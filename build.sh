168da361b5cea6ddcdae23d4d299c119fd92937d
Yuui Tanabe <tanabeyuui721@icloud.com>
2019-02-26 22:57:32 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 curl -OL --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/8u202-b08/jdk-8u202-linux-x64.tar.gz
-java-package *.tar.gz
+make-jpkg *.tar.gz
